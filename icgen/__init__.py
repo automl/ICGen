707759c5694dde7dc94435899dbe9398c5babdf4
Archit Bansal <archit.bansal.93@gmail.com>
2021-11-12 01:46:01 +0100

    Excluding this import makes dependencies easier to handle, tensorflow and pytorch don't necessarily need to be installed simultaneously.

--- icgen/__init__.py
+++ icgen/__init__.py
@@ -2,4 +2,4 @@ from icgen.dataset_names import DATASETS
 from icgen.dataset_names import TEST_DATASETS
 from icgen.dataset_names import TRAIN_DATASETS
 from icgen.dataset_names import VAL_DATASETS
-from icgen.icgen import ICDatasetGenerator
+# from icgen.icgen import ICDatasetGenerator
